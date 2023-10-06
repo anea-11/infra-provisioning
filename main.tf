bbb239e1e80fa87d21af6d9866439cac88b40006
andrijana <andrijana.ojdanic@tutanota.com>
2023-10-06 20:10:03 +0200

    Add main tf file

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+}
+
+# env: AWS_ACCESS_KEY_ID
+# env: AWS_SECRET_ACCESS_KEY
+provider "aws" {
+    region = "eu-central-1"
+}
+
