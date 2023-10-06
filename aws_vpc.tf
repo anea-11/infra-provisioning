b22388e219d97253fe2f1027aee7348f42f19b3c
andrijana <andrijana.ojdanic@tutanota.com>
2023-10-06 21:37:35 +0200

    Rename encoding vpc

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -10,12 +10,12 @@ resource "aws_vpc" "development_vpc" {
   }
 }
 
-resource "aws_vpc" "testing_p1_vpc" {
+resource "aws_vpc" "encoding_vpc" {
   cidr_block       = "172.17.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "testing_p1_vpc"
+    Name = "encoding_vpc"
   }
 }
 
