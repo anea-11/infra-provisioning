8f4ce0551ff47b23103386d474bbc038111e09d2
andrijana <andrijana.ojdanic@tutanota.com>
2023-10-06 21:27:44 +0200

    Add dev subnet

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -1,3 +1,6 @@
+####################################################################
+# VPCs
+####################################################################
 resource "aws_vpc" "development_vpc" {
   cidr_block       = "172.16.0.0/16"
   instance_tenancy = "default"
@@ -14,4 +17,17 @@ resource "aws_vpc" "testing_p1_vpc" {
   tags = {
     Name = "testing_p1_vpc"
   }
+}
+
+####################################################################
+# SUBNETS
+####################################################################
+resource "aws_subnet" "dev_utility_subnet" {
+    vpc_id              = aws_vpc.development_vpc.id
+    cidr_block          = "172.16.0.0/24"
+    availability_zone   = "eu-central-1c"
+
+    tags = {
+        Name = "dev_utility_subnet"
+    }
 }
