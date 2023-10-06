6adb6f149a2f9c400f4dd6a28b31e53b530a6ed6
andrijana <andrijana.ojdanic@tutanota.com>
2023-10-07 01:15:07 +0200

    Add security groups to allow access to servers

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "jenkins-server" {
   ami           = "ami-0329d3839379bfd15"
   instance_type = "t4g.small"
   subnet_id     = aws_subnet.dev_utility_subnet.id
+  security_groups = [aws_security_group.development_ssh_ingress.id]
 
   tags = {
     Name = "jenkins-server"
@@ -15,6 +16,7 @@ resource "aws_instance" "nexus-server" {
   ami           = "ami-0329d3839379bfd15"
   instance_type = "t4g.medium"
   subnet_id     = aws_subnet.dev_utility_subnet.id
+  security_groups = [aws_security_group.development_ssh_ingress.id]
 
   tags = {
     Name = "nexus-server"
@@ -28,6 +30,7 @@ resource "aws_instance" "encoding-server-1" {
   ami           = "ami-0329d3839379bfd15"
   instance_type = "t4g.small"
   subnet_id     = aws_subnet.encoding_app_subnet.id
+  security_groups = [aws_security_group.encoding_ssh_ingress.id, aws_security_group.encoding_frontend_ingress.id]
 
   tags = {
     Name = "encoding-server-1"
---
+++ aws_security_groups.tf
@@ -0,0 +1,46 @@
+resource "aws_security_group_rule" "development_ssh_ingress_rule" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.development_ssh_ingress.id
+}
+resource "aws_security_group" "development_ssh_ingress" {
+  name        = "development_ssh_ingress"
+  description = "Allow SSH access in development VPC"
+  vpc_id      = aws_vpc.development_vpc.id
+}
+######################################################################
+
+resource "aws_security_group_rule" "encoding_ssh_ingress_rule" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.encoding_ssh_ingress.id
+}
+
+resource "aws_security_group" "encoding_ssh_ingress" {
+  name        = "encoding_ssh_ingress"
+  description = "Allow SSH access in encoding VPC"
+  vpc_id      = aws_vpc.encoding_vpc.id
+}
+######################################################################
+
+resource "aws_security_group_rule" "encoding_frontend_ingress_rule" {
+  type        = "ingress"
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.encoding_frontend_ingress.id
+}
+
+resource "aws_security_group" "encoding_frontend_ingress" {
+  name        = "encoding_frontend_ingress"
+  description = "Access encoding app frontend"
+  vpc_id      = aws_vpc.encoding_vpc.id
+}
+
