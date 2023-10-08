16a34382cbf93903590265a0507635d579449f2a
andrijana <andrijana.ojdanic@tutanota.com>
2023-10-08 02:35:59 +0200

    Configure route tables for subnets

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -62,3 +62,46 @@ resource "aws_internet_gateway" "encoding_vpc_gateway" {
     Name = "encoding_vpc_gateway"
   }
 }
+
+#######################################################################
+# ROUTE TABLES
+#######################################################################
+resource "aws_route_table" "development_route_table" {
+  vpc_id = aws_vpc.development_vpc.id
+
+  tags = {
+    Name = "development_route_table"
+  }
+}
+resource "aws_route" "development_route_1" {
+  route_table_id         = aws_route_table.development_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.development_vpc_gateway.id
+}
+
+
+resource "aws_route_table" "encoding_route_table" {
+  vpc_id = aws_vpc.encoding_vpc.id
+
+  tags = {
+    Name = "encoding_route_table"
+  }
+}
+resource "aws_route" "encoding_route_1" {
+  route_table_id         = aws_route_table.encoding_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.encoding_vpc_gateway.id
+}
+
+#######################################################################
+# SUBNET-ROUTE TABLE ASSOCIATIONS
+#######################################################################
+resource "aws_route_table_association" "development_route_table_association" {
+  subnet_id      = aws_subnet.dev_utility_subnet.id
+  route_table_id = aws_route_table.development_route_table.id
+}
+
+resource "aws_route_table_association" "encoding_route_table_association" {
+  subnet_id      = aws_subnet.encoding_app_subnet.id
+  route_table_id = aws_route_table.encoding_route_table.id
+}
