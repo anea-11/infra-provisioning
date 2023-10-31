bf48d0b2b2de93abb01e8fcd0d089261a71f17aa
andrijana <andrijana.ojdanic@tutanota.com>
2023-10-31 01:28:44 +0100

    Format variables file

--- variables.tf
+++ variables.tf
@@ -5,13 +5,25 @@ variable "aws_region" {
 }
 
 variable "cicd_vpc_cidr_block" {
-    description = "CIDR block for the VPC in which CICD resources will be deployed"
-    type        = string
-    default     = "172.16.0.0/16"
+  description = "CIDR block for the VPC in which CICD resources will be deployed"
+  type        = string
+  default     = "172.16.0.0/16"
+}
+
+variable "cicd_vpc_name" {
+  description = "Name for the VPC in which CICD resources will be deployed"
+  type        = string
+  default     = "cicd_vpc"
+}
+
+variable "cicd_az" {
+  description = "Availability zone in which the resources will be deployed"
+  type        = string
+  default     = "eu-central-1c"
 }
 
 variable "encoding_app_vpc_cidr_block" {
-    description = "CIDR block for the VPC in which encoding-app resources will be deployed"
-    type        = string
-    default     = "172.17.0.0/16"
+  description = "CIDR block for the VPC in which encoding-app resources will be deployed"
+  type        = string
+  default     = "172.17.0.0/16"
 }
