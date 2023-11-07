2decf8488f3b9138387b7aa8084ba021315291f2
andrijana <andrijana.ojdanic@tutanota.com>
2023-11-08 00:47:52 +0100

    Remove aws-auth configmap management from tf configs

--- modules/aws_google_online_boutique/main.tf
+++ modules/aws_google_online_boutique/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.19.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.23.0"
-    }
   }
 }
 
@@ -63,42 +59,7 @@ module "google_online_boutique_eks" {
   }
 
   cluster_endpoint_public_access = true
-
-  manage_aws_auth_configmap = true
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::474024458802:root"
-      username = "root"
-      groups   : [
-        "system:nodes", "system:masters"
-      ]
-    }
-  ]
 }
-
-########################################################################################################
-# IMPORTANT: following code block will give an error when EKS cluster is not yet initialized!
-# In other words, it throws an error if terraform apply needs to create the EKS cluster.
-# Once the cluster already exists, the code works fine and athenticates with the cluster so that aws-auth configmap can be updated.
-# Workaround: comment-out the problematic code -> run terraform apply -> uncomment the problematic code-> run terraform apply again
-
-data "aws_eks_cluster" "default" {
-  name = module.google_online_boutique_eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "default" {
-  name = module.google_online_boutique_eks.cluster_name
-}
-
-# Authenticate with the cluster to be able to update aws-auth configmap with tf
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.default.token
-}
-
-# end of code that gives errors when EKS cluster is not yet initialized
-########################################################################################################
 
 module "google_online_boutique_efs" {
   source  = "terraform-aws-modules/efs/aws"
